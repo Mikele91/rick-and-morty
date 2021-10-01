b90bd1154268214f86a6a784c03c6c011b92f5e1
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-01 15:38:27 +0200

    struttura base cpmponenti

---
+++ src/components/Character.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="character">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Character",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Characters.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="characters">
+        <Character/>
+    </section>
+</template>
+
+<script>
+import Character from './Character.vue';
+export default { 
+    name: "Characters",
+    components: {
+        Character,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
